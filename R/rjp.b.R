0e634bc12f117ce2e02971265320a01cacdebcd9
Jonathon Love <jon@thon.cc>
2022-08-16 15:31:26 +1000

    Fix to Rj+ analyses loading as Rj analyses

--- R/rjp.b.R
+++ R/rjp.b.R
@@ -4,5 +4,11 @@ RjpClass <- R6::R6Class(
     inherit = RjClass,
     active=list(
         saveColumns=function() TRUE
+    ),
+    public=list(
+        initialize=function(...) {
+            super$initialize(...)
+            private$.name <- 'Rjp'
+        }
     )
 )
